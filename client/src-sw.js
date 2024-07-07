c9773073da48a6b712f493766fc6899d1a0e975f
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-07-06 19:01:39 -0500

    service worker logic

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,26 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // defining the criteria for the files we'll cache
+  ({ request }) => ['style', 'script', 'image'].includes(request.destination),
+  // directing the user to try to grab asset from cache first and then network second if none found
+  new CacheFirst({
+    // defining name of cached assets
+    cacheName: 'asset-cache',
+    // plugins for caching
+    plugins: [
+      // only caching successfully loaded assets
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // defining when cached assets will expire
+      new ExpirationPlugin({
+        // 10 days
+        maxAgeSeconds: 10 * 24 * 60 * 60,
+        // only 60 cached assets at once in order to limit storage use
+        maxEntries: 60,
+      }),
+    ],
+  })
+);
